81996b6c4dd84f26d8ab34bfc1c3bd92f3492346
Nadeem(gmail) <mohammed.nadeem55@gmail.com>
2015-09-04 11:36:23 +0530

    LP and TMB

--- TMB/assets/js/core.js
+++ TMB/assets/js/core.js
@@ -34,4 +34,44 @@ $(document).ready(function(){
 		width: '100%'
 	});
 
+	if($(".destination-timeline ul li").length == 1)
+	{
+		$(".destination-timeline ul li .desthrtimeline").hide();
+	}
+	if($(".destination-timeline ul li").length == 9)
+	{
+		$(".scrollbar").hide();
+	}
+
+	$('.open-summary').click(function(){
+      $('#summary-content').slideDown('2000', "swing", function () {
+        // Animation complete.
+      });
+    });
+    $('#summary-content .slidetop').click(function(){
+      $('#summary-content').slideUp('2000', "swing", function () {
+        // Animation complete.
+      });
+    });
+
 });
+
+function cloneDiv(divID) {
+      var id = document.getElementById("sectioncont").value;
+      id++;
+      document.getElementById("sectioncont").value = id;
+
+      var newDiv = $(divID).clone();
+      newDiv.attr("sectioncont", 'div' + id);
+
+      newDiv.children()[0].id = 'input' + id;
+      $("#button1").hide();
+      newDiv.children()[1].id = 'button' + id;
+      newDiv.children()[1].innerHTML = 'X';
+      newDiv.children()[1].onclick = function() {
+        $(this.parentNode).remove();
+        $("#button1").show();
+      };
+
+      newDiv.appendTo(".addingtravelhotel");
+    }
