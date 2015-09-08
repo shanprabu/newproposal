cacbb760e18f2aef0843aa0b54889cff93c4a622
Nadeem(gmail) <mohammed.nadeem55@gmail.com>
2015-09-08 20:09:15 +0530

    flight-proposal

--- flight-proposal/js/script.js
+++ flight-proposal/js/script.js
@@ -1,4 +1,31 @@
 jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.to-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('visible') : $back_to_top.removeClass('visible');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('visible');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+
 	//Navigation bar
 	$nav_items = $('#wrapper header nav ul.nav.nav-tabs > li > a');
 	$nav_items.on('click', function() {
@@ -42,7 +69,7 @@ jQuery(document).ready(function($){
 		$(this).parent().remove();
 		// console.log($(this).parent().html());
 		$('.add-city').show();
-		if ($('#multiCity-form .multicity-grp').length == 2) {
+		if ($('#multiCity-form .multicity-grp').length <= 2) {
 			$('.remove-city').hide();
 		};
 	});
