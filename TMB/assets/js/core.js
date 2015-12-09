5c13673a54fb0c2f425d27c5086554ae7e7ab931
Nadeem(gmail) <mohammed.nadeem55@gmail.com>
2015-12-09 14:12:24 +0530

    TMB Version 15

--- TMB/assets/js/core.js
+++ TMB/assets/js/core.js
@@ -1,77 +1,113 @@
 $(document).ready(function(){
 
-  	// Tool tip js starts 
-  	Tipped.create('.tool-tip');
-    // Tool tip js ends 
+jQuery('.scrollbar-inner').scrollbar();
 
-     // Date Picker
-     $(".dob").datepicker({
-     	dateFormat: 'dd M yy',
-     	autoclose:true,
-     	showOtherMonths: true,
-     	selectOtherMonths: true
-     }).attr('readonly', 'readonly');
+// Tool tip js starts 
+Tipped.create('.tool-tip');
+// Tool tip js ends 
 
-	// Date Picker
-	$(".start-date").datepicker({
-		dateFormat: 'dd M yy',
-		autoclose:true,
-		showOtherMonths: true,
-		selectOtherMonths: true
-	})
-	.attr('readonly', 'readonly');
-	$(".end-date").datepicker({
-		dateFormat: 'dd M yy',
-		autoclose:true,
-		showOtherMonths: true,
-		selectOtherMonths: true
-	})
-	.attr('readonly', 'readonly');
+// Date Picker
+$(".dob").datepicker({
+  dateFormat: 'dd M yy',
+  autoclose:true,
+  showOtherMonths: true,
+  selectOtherMonths: true
+}).attr('readonly', 'readonly');
 
-	$('#intrnalserv').change(function() {
-		console.log($(this).val());
-	}).multipleSelect({
-		width: '100%'
-	});
+// Date Picker
+$(".start-date").datepicker({
+  dateFormat: 'dd M yy',
+  autoclose:true, 
+  showOtherMonths: true,
+  selectOtherMonths: true
+})
+.attr('readonly', 'readonly');
+$(".end-date").datepicker({
+  dateFormat: 'dd M yy',
+  autoclose:true,
+  showOtherMonths: true,
+  selectOtherMonths: true
+})
+.attr('readonly', 'readonly');
 
-	if($(".destination-timeline ul li").length == 1)
-	{
-		$(".destination-timeline ul li .desthrtimeline").hide();
-	}
-	if($(".destination-timeline ul li").length == 9)
-	{
-		$(".scrollbar").hide();
-	}
+$('#intrnalserv').change(function() {
+  console.log($(this).val());
+}).multipleSelect({
+  width: '100%'
+});
 
-	$('.open-summary').click(function(){
-      $('#summary-content').slideDown('2000', "swing", function () {
-        // Animation complete.
+if($(".destination-timeline ul li").length == 1)
+{
+  $(".destination-timeline ul li .desthrtimeline").hide();
+}
+if($(".destination-timeline ul li").length == 9)
+{
+  $(".scrollbar").hide();
+}
+
+$('.open-summary').click(function(){
+  $('#summary-content').slideDown('2000', "swing", function () {
+// Animation complete.
+});
+});
+$('#summary-content .slidetop').click(function(){
+  $('#summary-content').slideUp('2000', "swing", function () {
+// Animation complete.
+});
+});
+
+
+$('.open-txt-summary').click(function(){
+  $('#txt-summary-content').slideDown('2000', "swing", function () {
+// Animation complete.
+});
+});
+$('#txt-summary-content .txt-slidetop').click(function(){
+  $('#txt-summary-content').slideUp('2000', "swing", function () {
+// Animation complete.
+});
+});
+
+
+$('#list').sortable({
+  scroll: true,
+  placeholder: 'placeholder',
+  containment: 'parent',
+        //axis: 'x'
       });
-    });
-    $('#summary-content .slidetop').click(function(){
-      $('#summary-content').slideUp('2000', "swing", function () {
-        // Animation complete.
-      });
-    });
+$('#list').disableSelection();
+
+
+
+
+
+
+
+
 
 });
 
 function cloneDiv(divID) {
-      var id = document.getElementById("sectioncont").value;
-      id++;
-      document.getElementById("sectioncont").value = id;
+  var id = document.getElementById("sectioncont").value;
+  id++;
+  document.getElementById("sectioncont").value = id;
 
-      var newDiv = $(divID).clone();
-      newDiv.attr("sectioncont", 'div' + id);
+  var newDiv = $(divID).clone();
+  newDiv.attr("sectioncont", 'div' + id);
 
-      newDiv.children()[0].id = 'input' + id;
-      $("#button1").hide();
-      newDiv.children()[1].id = 'button' + id;
-      newDiv.children()[1].innerHTML = 'X';
-      newDiv.children()[1].onclick = function() {
-        $(this.parentNode).remove();
-        $("#button1").show();
-      };
+  newDiv.children()[0].id = 'input' + id;
+  $("#button1").hide();
+  newDiv.children()[1].id = 'button' + id;
+  newDiv.children()[1].innerHTML = 'X';
+  newDiv.children()[1].onclick = function() {
+    $(this.parentNode).remove();
+    $("#button1").show();
+  };
 
-      newDiv.appendTo(".addingtravelhotel");
-    }
+  newDiv.appendTo(".addingtravelhotel");
+}
+
+
+
+
+
