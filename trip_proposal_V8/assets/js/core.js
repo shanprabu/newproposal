e87a2e97e29021b3dc0163094a7f8634bcb2c44a
lohithkumarnadella <lohith.kumar@compassitesinc.com>
2016-04-14 15:25:50 +0530

    Feedback form Changes- removed trust pilot

--- trip_proposal_V8/assets/js/core.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($){
-	$(".accordion").smk_Accordion({closeAble: true});
-});
--- trip_proposal_version1_V7_LK/travel_files/booktab.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.passengers-list').on('click', '.passengers-list-item-details', function(e) {
-		console.log($(this).parents('.dropdown-menu'));
-		var element = $(this).clone();
-		$(this).parents('.dropdown-menu').siblings('.dropdown-toggle').html(element);
-	});
-});
